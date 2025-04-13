c024dd2ee5f6a5cbd1af18636283eaaf8a776bc8
Sahil7475 <sahiljadhav7475@gmail.com>
2025-04-14 00:48:48 +0530

    [minor change]

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ export const register = async (req, res) => {
 
     const existing = await User.findOne({ email });
     if (existing) {
-      return res.status(409).json({ error: "Email already registered" });
+      return res.status(409).json({ error: "Email already registered!!" });
     }
 
     const hashed = await bcrypt.hash(password, 10);
